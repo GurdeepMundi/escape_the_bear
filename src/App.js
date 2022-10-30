661fadb53051fbc9534c392b6ab8006f1d5bcc80
pled2720 <d.pleskach@uleth.ca>
2022-10-29 22:26:08 -0600

    new functionalcomponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './forest_map.jpg';
 import './App.css';
+import Game from './Game.js' 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Game logo={logo}/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Game = (props) => {
+    return (
+        <div>
+            <img src={props.logo} className="App-logo" alt="logo" />
+        </div>
+    )
+}
+
+
+export default Game;
