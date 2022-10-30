8d5f97a9b2dc745abd02d0df25287b5eb954e131
Gurdeep Singh <gurdeepmundi10@gmail.com>
2022-10-30 15:01:05 -0600

    removing react stuff

--- src/App.js
+++ src/App.js
@@ -9,17 +9,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Game logo={logo} player={player}/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
