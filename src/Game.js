843b9343a8d862ab2604f42911f7622abe90f8e2
pled2720 <d.pleskach@uleth.ca>
2022-10-30 17:17:30 -0600

    bear chasing

--- src/Game.js
+++ src/Game.js
@@ -10,8 +10,26 @@ const Game = (props) => {
     
     const [playerTop, setPTop] = useState(220);
     const [playerLeft, setPLeft] = useState(430);
-    
-    
+    const [range, setRange] = useState(50);
+    let [distance, setDistance] = useState(0);
+    function moveRight() {
+        setPLeft(playerLeft + 50)
+        setDistance(distance + 1);
+        if(distance === 8) {
+            const answer = prompt("What is \"1\" + \"1\"");
+            if(answer === "11"){
+                alert("You escaped the bear!");
+                setRange(300)
+            } else {
+                alert("The bear caught you!");
+                setRange(0)
+            }
+        }
+    }
+    function moveLeft() {
+        setPLeft(playerLeft - 50);
+        setDistance(distance - 1);
+    }
     return (
         <div>
             <img src={props.player}
@@ -27,12 +45,12 @@ const Game = (props) => {
                 height: "10vmin",
                 position: "absolute",
                 top: `${playerTop}px`,
-                left: `${playerLeft - 50}px`
+                left: `${playerLeft - range}px`
             }}
             alt="bear"/>
             <img src={props.logo} className="App-logo" alt="logo" 
                 />
-            <button onClick={()=> setPLeft(playerLeft - 50)}><img src={arrowLeft} 
+            <button onClick={()=> moveLeft()}><img src={arrowLeft} 
                 
                 style={{
                 height: "10vmin"}} alt="left"/></button>
@@ -46,7 +64,7 @@ const Game = (props) => {
                 style={{
                 height: "10vmin"}} alt="down"/>
             </button>
-            <button onClick={()=> setPLeft(playerLeft + 50)}>
+            <button onClick={()=> moveRight()}>
             <img src={arrowRight} 
                 
                 style={{
